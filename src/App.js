fd58516322e0b1169038e066ba37532da63745cd
“rubeniriso” <rubenirisoe@gmail.com>
2022-06-13 11:46:55 +0200

    [#1] - Sign up frontend prototype

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Router, Routes, Route } from "react-router"
+import { useLocation } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Signup from "./pages/Signup";
+import Kanji from "./pages/Kanji";
 import Header from "./components/Header";
 
 function App() {
+  const location = useLocation();
   return (
     <>
-      <Router>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Dashboard />}></Route>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/register" element={<Register />}></Route>
-          </Routes>
-        </div>
-      </Router>
+    {(location.pathname !== "/login" && location.pathname !== "/signup" && location.pathname !== 'error' && location.pathname !== '/404') && <Header />}
+        <Routes>
+          <Route path="/" element={<Dashboard />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+          <Route path="/study/kanji" element={<Kanji />}></Route>
+        </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Header />
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
